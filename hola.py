30463ec0bae23ac0d37bd09b39948db2fc54137e
vicentepinilla <vicentepinillas3@gmail.com>
2025-04-16 16:40:43 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,45 @@
+# nombre="Vicente"
+# edad=18
+# print ("hola", nombre, "su edad es", edad)
+
+# print ("ingrese su nombre")
+# nombre=input()
+# print("ingrese su edad")
+# edad=input()
+# print("hola"),nombre, ("su edad es",edad)
+
+# print("Ingrese dos numeros")
+# n1=int(input ())
+# n2=int(input())
+# print("La suma de los numeros es", n1+n2 )
+
+# print ("ingrese 3 numeros")
+# n1=int (input()) 
+# n2=int (input())
+# n3=int (input())
+# promedio=(n1+n2+n3)/3
+# print ("el promedio final es ",promedio)
+
+# if promedio<40:
+#     print("el alumno aprobo")
+# else:    
+    # print("el alumno reprobo")
+
+
+# edad=int(input("ingrese su edad"))
+# if edad>=18:
+#          print ("usted es mayor de edad")
+# else:
+#          print("ustes es menor de edad")
+
+# edad=int(input("ingrese su edad"))
+# if edad<12:
+#          print ("usted es un niño")
+# elif edad>=12 and edad<18:
+#        print ("usted es un adolescente")
+# elif edad>=18 and edad <65:
+#         print ("usted es un adulto")
+# else  print ("usted es un tatita")
+
+
+
